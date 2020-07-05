b841eacd8f4e336fcd8e7bae5592351396719c5c
Chris Bacon <chrisbacon2009@hotmail.com>
2020-07-05 15:18:05 +0100

    Added minor error handling

--- handler.js
+++ handler.js
@@ -7,62 +7,76 @@ const Calculator = require("./Calculator");
 let inMemoryCache = {};
 
 module.exports.api = async event => {
-  const principal = parseInt(event.queryStringParameters.principal);
-  const monthlyAmount = parseInt(event.queryStringParameters.monthlyAmount);
-  const interestRate = parseFloat(event.queryStringParameters.interestRate);
-  const termLength = parseInt(event.queryStringParameters.termLength);
-  const compoundsPerYear = parseInt(event.queryStringParameters.compoundsPerYear);
+  try {
+    const principal = parseInt(event.queryStringParameters.principal);
+    const monthlyAmount = parseInt(event.queryStringParameters.monthlyAmount);
+    const interestRate = parseFloat(event.queryStringParameters.interestRate);
+    const termLength = parseInt(event.queryStringParameters.termLength);
+    const compoundsPerYear = parseInt(event.queryStringParameters.compoundsPerYear);
 
-  const hashedParams = md5(`${principal}${monthlyAmount}${interestRate}${termLength}${compoundsPerYear}`);
+    const hashedParams = md5(`${principal}${monthlyAmount}${interestRate}${termLength}${compoundsPerYear}`);
 
-  const cacheHit = inMemoryCache[hashedParams];
+    const cacheHit = inMemoryCache[hashedParams];
 
-  let calculationResult;
+    let calculationResult;
 
-  if (cacheHit) {
-    calculationResult = {
-      cache: "hit",
-      value: cacheHit.value,
-      history: cacheHit.history,
+    if (cacheHit) {
+      calculationResult = {
+        cache: "hit",
+        value: cacheHit.value,
+        history: cacheHit.history,
+      }
+    } else {
+      const calculator = new Calculator(principal, monthlyAmount, interestRate, compoundsPerYear);
+
+      const history =
+        [...Array(termLength - 1).keys()]
+          .map(i => ({ year: i + 1, value: calculator.calculate(i + 1) }));
+
+      const value = calculator.calculate(termLength);
+
+      inMemoryCache[hashedParams] = { value, history };
+
+      calculationResult = {
+        cache: "miss",
+        value: value,
+        history: history,
+      };
     }
-  } else {
-    const calculator = new Calculator(principal, monthlyAmount, interestRate, compoundsPerYear);
 
-    const history =
-      [...Array(termLength - 1).keys()]
-        .map(i => ({ year: i + 1, value: calculator.calculate(i + 1) }));
-
-    const value = calculator.calculate(termLength);
-
-    inMemoryCache[hashedParams] = { value, history };
-
-    calculationResult = {
-      cache: "miss",
-      value: value,
-      history: history,
-    };
-  }
-
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
-      "Cache-Control": "no-cache",
-    },
-    body: JSON.stringify(
-      {
-        ...calculationResult,
-        input: {
-          principal,
-          monthlyAmount,
-          interestRate,
-          termLength,
-          compoundsPerYear
-        },
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Cache-Control": "no-cache",
       },
-      null,
-      2
-    ),
-  };
+      body: JSON.stringify(
+        {
+          ...calculationResult,
+          input: {
+            principal,
+            monthlyAmount,
+            interestRate,
+            termLength,
+            compoundsPerYear
+          },
+        },
+        null,
+        2
+      ),
+    };
+  } catch (error) {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Cache-Control": "no-cache",
+      },
+      body: JSON.stringify({
+        msg: `${error}`
+      }),
+    }
+  }
 };
