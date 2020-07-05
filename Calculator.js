08e753c98fbb4c5939d359f6ecdd02acb007c1ce
Chris Bacon <chrisbacon2009@hotmail.com>
2020-07-05 01:29:36 +0100

    Add comment about why there is two formulas

--- Calculator.js
+++ Calculator.js
@@ -21,6 +21,9 @@ class Calculator {
       return this.monthlyAmount * ((Math.pow(interestRatePerCompoundPlusOne, (this.compoundsPerYear * termLength)) - 1) / interestRatePerCompound) * interestRatePerCompoundPlusOne;
     }
 
+
+    // https://www.thecalculatorsite.com/articles/finance/compound-interest-formula.php
+    // Important note: this compound interest formula for different periodic payments only works if the number of compounds per year is equal to or greater than the number of contributions per year. For more information about what to do when the payment period doesn't match the compound period, see this useful page from Jon Wittwer: https://www.vertex42.com/Calculators/compound-interest-calculator.html#rate-per-period
     const rate = ((1 + this.interestRate / this.compoundsPerYear) ** (this.compoundsPerYear / paymentPeriodsPerYear)) - 1;
     const totalNumberOfPayments = paymentPeriodsPerYear * termLength;
 
